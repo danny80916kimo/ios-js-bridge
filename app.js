15fc6b05c84c9cec04ef8702c2f5fdf7b34fd4de
RyanLiou <ryanliou@wondercore.com>
2022-04-16 22:33:24 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("test")
+
+
+function setupWKWebViewJavascriptBridge(callback) {
+    if (window.WKWebViewJavascriptBridge) { return callback(WKWebViewJavascriptBridge); }
+    if (window.WKWVJBCallbacks) { return window.WKWVJBCallbacks.push(callback); }
+    window.WKWVJBCallbacks = [callback];
+    window.webkit.messageHandlers.iOS_Native_InjectJavascript.postMessage(null)
+}
+
+setupWKWebViewJavascriptBridge(function(bridge) {
+
+	/* Initialize your app here */
+
+	bridge.registerHandler('testJavascriptHandler', function(data, responseCallback) {
+		console.log('iOS called testJavascriptHandler with', data)
+		responseCallback({ 'Javascript Says':'Right back atcha!' })
+	})
+
+	bridge.callHandler('App.back();', {'foo': 'bar'}, function(response) {
+		console.log('JS got response', response)
+	})
+})
