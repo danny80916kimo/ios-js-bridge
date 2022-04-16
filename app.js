80c7d74019912b0d2285e8f59a6d4812b934a57f
RyanLiou <ryanliou@wondercore.com>
2022-04-16 23:19:37 +0800

    remove wrong js code

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ function great(){
 
 great();
 
-window.webkit.messageHandlers.great.postMessage("Send message to Swift")
+// window.webkit.messageHandlers.great.postMessage("Send message to Swift")
 
 
-function setupWKWebViewJavascriptBridge(callback) {
-    if (window.WKWebViewJavascriptBridge) { return callback(WKWebViewJavascriptBridge); }
-    if (window.WKWVJBCallbacks) { return window.WKWVJBCallbacks.push(callback); }
-    window.WKWVJBCallbacks = [callback];
-    window.webkit.messageHandlers.iOS_Native_InjectJavascript.postMessage(null)
-}
+// function setupWKWebViewJavascriptBridge(callback) {
+//     if (window.WKWebViewJavascriptBridge) { return callback(WKWebViewJavascriptBridge); }
+//     if (window.WKWVJBCallbacks) { return window.WKWVJBCallbacks.push(callback); }
+//     window.WKWVJBCallbacks = [callback];
+//     window.webkit.messageHandlers.iOS_Native_InjectJavascript.postMessage(null)
+// }
 
-setupWKWebViewJavascriptBridge(function(bridge) {
+// setupWKWebViewJavascriptBridge(function(bridge) {
 
-	/* Initialize your app here */
+// 	/* Initialize your app here */
 
-	bridge.registerHandler('testJavascriptHandler', function(data, responseCallback) {
-		console.log('iOS called testJavascriptHandler with', data)
-		responseCallback({ 'Javascript Says':'Right back atcha!' })
-	})
+// 	bridge.registerHandler('testJavascriptHandler', function(data, responseCallback) {
+// 		console.log('iOS called testJavascriptHandler with', data)
+// 		responseCallback({ 'Javascript Says':'Right back atcha!' })
+// 	})
 
-	bridge.callHandler('App.back();', {'foo': 'bar'}, function(response) {
-		console.log('JS got response', response)
-	})
-})
+// 	bridge.callHandler('App.back();', {'foo': 'bar'}, function(response) {
+// 		console.log('JS got response', response)
+// 	})
+// })
 
